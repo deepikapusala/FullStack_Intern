3b518b0c9527a1f73e5bc196bba96f8bd2327452
Deepika <138294754+deepikapusala@users.noreply.github.com>
2026-08-20 15:28:12 +0530

    Week 03 py data model mutability scope (#8)
    * task 1 reporting!
    
    * feat: python files

---
+++ week-03-01/shallow_deep_copy.py
@@ -0,0 +1,31 @@
+# import copy
+# def show_copy_difference():
+
+#     original = [[1, 2], [3, 4]]
+#     print("Original:", original) 
+
+#     shallow = copy.copy(original)  
+#     print("Shallow copy:", shallow)
+    
+#     shallow[0].append(99)
+
+#     print("Shallow copy after modification:", shallow) 
+#     print("Original after shallow copy modification:", original) 
+
+# show_copy_difference()
+
+import copy
+def show_copy_difference():
+
+    original = [[1, 2], [3, 4]]
+    print("Original:", original) 
+
+    deepcopy = copy.deepcopy(original)  
+    print("Deep copy:", deepcopy)
+    
+    deepcopy[0].append(100)
+
+    print("Deep copy after modification:", deepcopy) 
+    print("Original after shallow copy modification:", original) 
+
+show_copy_difference()
