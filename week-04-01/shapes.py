2902777c1cbf8a87358f661b21a6b4ebc3553a6a
deepikapusala <138294754+deepikapusala@users.noreply.github.com>
2026-09-03 15:53:15 +0530

    Add OOP dunder methods examples

---
+++ week-04-01/shapes.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+# Polymorphism
+shapes = [
+    Circle(5),
+    Rectangle(10, 4)
+]
+
+for shape in shapes:
+    print(shape.area())
